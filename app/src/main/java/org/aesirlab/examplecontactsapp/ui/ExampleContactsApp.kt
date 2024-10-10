432eaa4785dd004ebd40f3902fe1606d75723ebc
Zach <zg009@uark.edu>
2024-10-10 12:57:30 -0500

    added navigation

--- app/src/main/java/org/aesirlab/examplecontactsapp/ui/ExampleContactsApp.kt
+++ app/src/main/java/org/aesirlab/examplecontactsapp/ui/ExampleContactsApp.kt
@@ -1,9 +1,38 @@
 package org.aesirlab.examplecontactsapp.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+
+enum class ContactsAppScreen {
+    MainScreen,
+    AddContactScreen,
+    DetailViewContactScreen
+}
 
 @Composable
 fun ExampleContactsApp() {
     val navController = rememberNavController()
+
+    Scaffold {
+        NavHost(
+            navController = navController,
+            startDestination = ContactsAppScreen.MainScreen.name,
+            modifier = Modifier.padding(it)
+        ) {
+            composable(route = ContactsAppScreen.MainScreen.name) {
+
+            }
+            composable(route = ContactsAppScreen.AddContactScreen.name) {
+
+            }
+            composable(route = ContactsAppScreen.DetailViewContactScreen.name) {
+
+            }
+        }
+    }
 }
