231526c0f70977d2463936c67cb33e593a849890
Zach <zg009@uark.edu>
2024-10-10 12:53:55 -0500

    starter

---
+++ app/src/main/java/org/aesirlab/examplecontactsapp/ui/ExampleContactsApp.kt
@@ -0,0 +1,9 @@
+package org.aesirlab.examplecontactsapp.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun ExampleContactsApp() {
+    val navController = rememberNavController()
+}
