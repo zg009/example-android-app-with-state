7c09d92497be4ce7083683312fc08a85630cfb83
Zach <zg009@uark.edu>
2024-10-10 14:09:22 -0500

    finished

--- app/src/main/java/org/aesirlab/examplecontactsapp/logic/ContactsViewModel.kt
+++ app/src/main/java/org/aesirlab/examplecontactsapp/logic/ContactsViewModel.kt
@@ -1,14 +1,11 @@
 package org.aesirlab.examplecontactsapp.logic
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import org.aesirlab.examplecontactsapp.model.Contact
-import java.util.Date
 
 class ContactsViewModel: ViewModel() {
 
--- app/src/main/java/org/aesirlab/examplecontactsapp/ui/screens/DetailViewContactScreen.kt
+++ app/src/main/java/org/aesirlab/examplecontactsapp/ui/screens/DetailViewContactScreen.kt
@@ -1,13 +1,32 @@
 package org.aesirlab.examplecontactsapp.ui.screens
 
+import android.icu.text.SimpleDateFormat
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.aesirlab.examplecontactsapp.model.Contact
+import java.util.Date
 
 @Composable
 fun DetailViewContactScreen(contact: Contact) {
-    Text(text = "This is the detail view screen!")
+//    Text(text = "This is the detail view screen!")
+    Column {
+        Spacer(modifier = Modifier.padding(vertical = 3.dp))
+        Text(text = contact.firstName)
+        Spacer(modifier = Modifier.padding(vertical = 3.dp))
+        Text(text = contact.lastName)
+        Spacer(modifier = Modifier.padding(vertical = 3.dp))
+        Text(text = contact.email)
+        Spacer(modifier = Modifier.padding(vertical = 3.dp))
+        Text(text = contact.phoneNumber)
+        Spacer(modifier = Modifier.padding(vertical = 3.dp))
+        Text(text = SimpleDateFormat("yyyy.MM.dd HH:mm").format(Date(contact.addedDate)))
+    }
 }
 
 @Preview
